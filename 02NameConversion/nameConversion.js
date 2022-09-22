eee250961ca334e8637d53acb96cef292251a35a
igniteshark <singhpardeep585@gmail.com>
2022-09-22 14:18:36 +0530

    Reducing js file size by adding an object function to code repetation

--- 02NameConversion/nameConversion.js
+++ 02NameConversion/nameConversion.js
@@ -17,70 +17,10 @@ convertBtn.addEventListener("click", function() {
     }
 })
 
-// const methods = {
-    // camelCase : function() {
-    //     const camelCase = input.value;
-    //    let camelCaseArr = camelCase.split(' ').map((word, index)=>{
-    //     if (index == 0) {
-    //       return word.toLowerCase();
-    //     }
-    //    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-    //    })
-    //     if (camelCaseArr.length == 2) {
-    //         return camelCaseArr.join("");
-    //     }
-    //    else{
-    //     return camelCaseArr.join(" ");
-    //    }
-    // },
-    // pasCalCase : function() {
-    //     const pasCalCase = input.value;
-    //    let pasCalCaseArr = pasCalCase.split(' ').map((word)=>{
-    //    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-    //    })
-    //     if (pasCalCaseArr.length == 2) {
-    //         return pasCalCaseArr.join("");
-    //     }
-    //    else{
-    //     return pasCalCaseArr.join(" ");
-    //    }
-    // },
-    // snakeCase : function() {
-    //     const snakeCase = input.value;
-    //    let snakeCaseArr = snakeCase.split(' ').map((word)=>{
-    //    return word.toLowerCase();
-    //    })
-    //      return snakeCaseArr.join("_");
-    // },
-    // screamingSnakeCase : function() {
-    //     const screamingSnakeCase = input.value;
-    //    let screamingSnakeCaseArr = screamingSnakeCase.split(' ').map((word)=>{
-    //    return word.toUpperCase();
-    //    })
-    //      return screamingSnakeCaseArr.join("_");
-    // },
-    // kebabCase : function() {
-    //     const kebabCase = input.value;
-    //    let kebabCaseArr = kebabCase.split(' ').map((word)=>{
-    //    return word.toLowerCase();
-    //    })
-    //      return kebabCaseArr.join("-");
-    // },
-    // screamingKebabCase : function() {
-    //     const screamingKebabCase = input.value;
-    //    let screamingKebabCaseArr = screamingKebabCase.split(' ').map((word)=>{
-    //    return word.toUpperCase();
-    //    })
-    //      return screamingKebabCaseArr.join("-");
-    // }
-
-// }
-
 const methods = {
     inputValue: function() {
         return input.value.split(' ');
     },
-
    camelCase : function() {
        let camelCaseArr = this.inputValue().map((word, index)=>{
         if (index == 0) {
