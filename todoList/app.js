addb0bf58e8beec513f1c79e39b0e97b9bef46ec
igniteshark <singhpardeep585@gmail.com>
2022-09-30 00:19:36 +0530

    intial commit created an todo app using javascript as soon all the functionality is ready but styling element are pending

---
+++ todoList/app.js
@@ -0,0 +1,117 @@
+const todoList = document.querySelector(".todo_list");
+const todoTitle = document.getElementById("todo_title");
+const todoText = document.getElementById("todo_text");
+const todoAddBtn = document.getElementById("add_todo_btn");
+
+window.addEventListener("load", getTodoList);
+function setTodoList() {
+    todoAddBtn.addEventListener("click", () => {
+        const todoNotes = localStorage.getItem("todos");
+        if (todoNotes == null) {
+            todos = [];
+        } else {
+            todos = JSON.parse(todoNotes);
+        }
+        const todoObj = new Object();
+        todoObj.title = todoTitle.value;
+        todoObj.text = todoText.value;
+        todos.push(todoObj);
+        localStorage.setItem("todos", JSON.stringify(todos));
+        getTodoList();
+        messageBox("Your todo has been created successfully");
+    });
+}
+setTodoList();
+
+// get todolist from localStorage and show
+function getTodoList() {
+    const todoNotes = localStorage.getItem("todos");
+    if (todoNotes == null) {
+        todoList.innerHTML = "Todo is empty please add notes";
+    } else {
+        const todoArr = JSON.parse(todoNotes);
+        // clear all strings then add new strings
+        todoList.innerHTML = '';
+        todoArr.forEach((element, index) => {
+            todoList.innerHTML += `
+        <div class="todo_content">
+            <h1 id="todo_title">${element.title}</h1>
+            <p id="todo_text">${element.text}</p>
+            <div class="edit_btn">
+            <input type="text" placeholder="Edit todo" class="edit_todo_input" hidden="true">
+            <button id="${index}" class="edit_todo_done" hidden="true">Done</button>
+            <button class="edit_todo">Edit</button>
+            <button id="${index}" onclick="removeBtnFunc(this.id)" class="todo_remove">Remove</button>
+            </div>
+        </div>
+        `
+        });
+    }
+    editTodoFunc();
+}
+
+
+
+// remove todo list item
+function removeBtnFunc(index) {
+    todoObj = localStorage.getItem("todos");
+    if (todoObj == null) {
+        todos = [];
+    }
+    else {
+        todos = JSON.parse(todoObj);
+    }
+    todos.splice(index, 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+    if (todos.length == 0) {
+        localStorage.clear();
+    }
+    getTodoList();
+    messageBox("Your todo has been removed successfully");
+}
+
+
+
+
+// edit todo list
+function editTodoFunc() {
+    const editBtn = document.querySelectorAll(".edit_todo");
+    const editInput = document.querySelectorAll(".edit_todo_input");
+    const doneEditTodo = document.querySelectorAll(".edit_todo_done");
+    editBtn.forEach((ele, index) => {
+        ele.addEventListener("click", () => {
+            doneEditTodo[index].hidden = false;
+            editInput[index].hidden = false;
+            editBtn[index].hidden = true;
+        });
+        doneEditTodo[index].addEventListener("click", () => {
+            const editInputText = editInput[index].value;
+            if (editInputText.length == 0) {
+                const input = editInput[index];
+                input.placeholder = "please enter text to edit todo";
+                input.classList.add("colorWhite");
+            } else {
+                let todoObj = localStorage.getItem("todos");
+                todos = JSON.parse(todoObj);
+                todos.splice(index, 1, {
+                    title: todos[index].title,
+                    text: editInputText,
+                });
+                localStorage.setItem("todos", JSON.stringify(todos));
+                getTodoList();
+                messageBox("Your todo has been edited successfully");
+            }
+        });
+    });
+}
+
+
+//message box
+function messageBox(message) {
+    const messageBox = document.querySelector(".message_box");
+    messageBox.innerHTML = message;
+    messageBox.hidden = false;
+    setTimeout(() => {
+        messageBox.hidden = true;
+    }, 3000);
+}
